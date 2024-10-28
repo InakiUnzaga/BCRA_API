bb68b8f1b3237d2bb3834a9779e491c4691f5b37
inakiUnzaga <inakiunzaga88@gmail.com>
2024-10-27 21:44:48 -0300

    Se incluyen los test

--- Test/test_Check_Connection_Redshift.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import psycopg2
-from psycopg2 import OperationalError
-import os
-
-
- 
-if __name__ == "__main__":
-    unittest.main()
---
+++ Test/test_conteo_columnas.py
@@ -0,0 +1,40 @@
+
+import unittest
+from unittest import mock
+import sys
+import os
+import pandas as pd
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Nivel_1.extraccion_bcra import Extraccion_Bcra
+
+
+class Test_extraccion(unittest.TestCase):
+
+    @mock.patch('requests.get')
+    def test_conteo_columnas(self, mock_get):
+        #simulamos la llamada a la api
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            "results": {
+                "detalle": [
+                    {"codigoMoneda": "USD", "tipoPase": 0.1, "tipoCotizacion": 0.2}
+                ],
+                "fecha": "27/10/2024"
+            }
+        }
+        #simulamos el objeto kwargs y ti para que se puede ejecutar la funcion
+        mock_ti = mock.Mock()
+        kwargs = {"ti": mock_ti}
+
+        ruta_parquet = Extraccion_Bcra(**kwargs)
+
+        df_resultado = pd.read_parquet(ruta_parquet)
+
+        #Verificamos que el numero total de columnas sean 4
+        self.assertEqual(len(df_resultado.columns), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
